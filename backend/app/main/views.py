a80374cf93f2e99e6ff5c086e089c1df10bc606f
NolanSun <shr1213@live.com>
2019-10-23 21:14:12 +0800

    favicon

--- backend/app/main/views.py
+++ backend/app/main/views.py
@@ -11,3 +11,11 @@ from flask_cors import cross_origin
 @main.route('/<path:path>')
 def catch_all(path):
     return render_template("index.html")
+
+@main.route('/favicon.ico', methods=['GET'])
+@cross_origin()
+def faviconico():
+    with open("C:\\auto_test\\CrmTools\\vue\\dist\\favicon.ico", 'rb') as f:
+        image = f.read()
+    resp = Response(image, mimetype="image/jpeg")
+    return resp
