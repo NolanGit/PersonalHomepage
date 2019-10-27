08521636225b5696423eb442b759eb67e69a09a8
NolanSun <shr1213@live.com>
2019-10-27 22:35:04 +0800

    1027

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -9,12 +9,12 @@
     <el-row class="cardRow">
       <el-col :span="7" :offset="1">
         <el-card shadow="hover">
-          <el-carousel autoplay="false" height="400px">
+          <el-carousel :autoplay="false" height="250px">
             <el-carousel-item v-for="city in cities" :key="city">
               <weather :city="city" />
             </el-carousel-item>
             <el-carousel-item v-show="user!=''">
-              <el-button icon="el-icon-circle-plus-outline" circle>主要按钮</el-button>
+              <el-button icon="el-icon-circle-plus-outline" circle></el-button>
             </el-carousel-item>
           </el-carousel>
         </el-card>
