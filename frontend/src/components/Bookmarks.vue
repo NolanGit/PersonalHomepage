b20c7a6fdae4466a9a1dbcd979ed954a6d8d18fd
NolanSun <shr1213@live.com>
2019-10-30 20:19:58 +0800

    20191030

--- frontend/src/components/Bookmarks.vue
+++ frontend/src/components/Bookmarks.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="bookmarks-main">
-    <div>{{user}}</div>
-    <div>{{bookmarks}}</div>
+    <el-row type="flex" justify="center">
+      <div>
+        <div class="bookmarks">书签</div>
+      </div>
+    </el-row>
     <el-row v-for="bookmarksSuite in bookmarksData" :key="bookmarksSuite">
       <el-col :span="6" v-for="bookmark in bookmarksSuite" :key="bookmark">
         <el-button
@@ -13,15 +16,24 @@
           <i :class="bookmark.icon" style="margin-right=5px;font-size=15px"></i>
           {{bookmark.name}}
         </el-button>
-        <el-button
-          class="bookmarkButton"
-          size="small"
-          @click="bookmarkAdd()"
-          v-show="bookmark.type==-1"
-        >
-          <i :class="bookmark.icon" style="margin-right=5px;font-size=15px"></i>
-          {{bookmark.name}}
-        </el-button>
+        <el-popover placement="top" width="260" v-model="bookmarkPopover.visible">
+          <p>添加城市：</p>
+          <el-input size="mini" v-model="bookmarkPopover.name" placeholder="网站名称"></el-input>
+          <el-input size="mini" v-model="bookmarkPopover.url" placeholder="链接(需要完整填写，包括'http://')"></el-input>
+          <el-input size="mini" v-model="bookmarkPopover.icon" placeholder="图标名称"></el-input>
+          <div style="text-align: right; margin: 0">
+            <el-button type="primary" size="mini" @click="addLocation()">确定</el-button>
+          </div>
+          <el-button
+            class="bookmarkButton"
+            size="small"
+            @click="bookmarkAdd()"
+            v-show="bookmark.type==-1"
+          >
+            <i :class="bookmark.icon" style="margin-right=5px;font-size=15px"></i>
+            {{bookmark.name}}
+          </el-button>
+        </el-popover>
       </el-col>
     </el-row>
   </div>
@@ -45,7 +57,13 @@ export default {
   data() {
     return {
       bookmarksData: [],
-      bookmarksDataTemp: []
+      bookmarksDataTemp: [],
+      bookmarkPopover: {
+        visible: false,
+        name: "",
+        url: "https://",
+        icon: ""
+      }
     };
   },
   methods: {
@@ -60,36 +78,36 @@ export default {
       if (this.bookmarksDataTemp == bookmarksData) {
         return;
       } else {
-        if (bookmarksData.length != 0) {
-          console.log("bookmarksData1");
-          console.log(bookmarksData);
-          if (bookmarksData[bookmarksData.length - 1].length == 4) {
-            bookmarksData.push([
-              {
-                icon: "el-icon-plus",
-                id: -1,
-                name: "增加",
-                update_time: "",
-                url: "",
-                type: -1
-              }
-            ]);
-          } else {
-            bookmarksData[bookmarksData.length - 1].push({
+        console.log("bookmarksData1");
+        console.log(bookmarksData);
+        if (
+          (bookmarksData[bookmarksData.length - 1].length == 4) |
+          (bookmarksData[bookmarksData.length - 1].length == 0)
+        ) {
+          bookmarksData.push([
+            {
               icon: "el-icon-plus",
               id: -1,
               name: "增加",
               update_time: "",
               url: "",
               type: -1
-            });
-          }
-          this.bookmarksData = bookmarksData;
-          this.bookmarksDataTemp = bookmarksData;
-          console.log("bookmarksData2");
-          console.log(bookmarksData);
+            }
+          ]);
         } else {
+          bookmarksData[bookmarksData.length - 1].push({
+            icon: "el-icon-plus",
+            id: -1,
+            name: "增加",
+            update_time: "",
+            url: "",
+            type: -1
+          });
         }
+        this.bookmarksData = bookmarksData;
+        this.bookmarksDataTemp = bookmarksData;
+        console.log("bookmarksData2");
+        console.log(bookmarksData);
       }
     }
   },
@@ -103,5 +121,12 @@ export default {
 .bookmarkButton {
   width: 90px;
   height: 40px;
+}
+.bookmarks {
+  font-family: Helvetica Neue, Helvetica, PingFang SC, Hiragino Sans GB,
+    Microsoft YaHei, SimSun, sans-serif;
+  font-weight: 600;
+  font-size: 20px;
+  color: #303133;
 }
 </style>
