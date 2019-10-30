ff44af7b296044893a1d5628122eedd961c7e8f1
NolanSun <shr1213@live.com>
2019-10-30 18:09:17 +0800

    20191030

--- frontend/src/components/Bookmarks.vue
+++ frontend/src/components/Bookmarks.vue
@@ -4,7 +4,18 @@
     <div>{{bookmarks}}</div>
     <el-row v-for="bookmarksSuite in bookmarksData" :key="bookmarksSuite">
       <el-col :span="6" v-for="bookmark in bookmarksSuite" :key="bookmark">
-        <el-button size="small" :icon="bookmark.icon" @click="buttonClicked(bookmark.url)">{{bookmark.name}}</el-button>
+        <el-button
+          size="small"
+          :icon="bookmark.icon"
+          @click="buttonClicked(bookmark.url)"
+          v-show="bookmark.type!=-1"
+        >{{bookmark.name}}</el-button>
+        <el-button
+          size="small"
+          :icon="bookmark.icon"
+          @click="bookmarkAdd()"
+          v-show="bookmark.type==-1"
+        >{{bookmark.name}}</el-button>
       </el-col>
     </el-row>
   </div>
@@ -34,10 +45,21 @@ export default {
     buttonClicked(bookmarkUrl) {
       console.log(bookmarkUrl);
       window.open(bookmarkUrl);
+    },
+    bookmarkAdd() {
+      console.log("要增加？");
     }
   },
   mounted() {
     console.log(this.bookmarks);
+    this.bookmarks[-1].push({
+      icon: "el-icon-plus",
+      id: -1,
+      name: "增加",
+      update_time: "",
+      url: "",
+      type: -1
+    });
   }
 };
 </script>
@@ -45,7 +67,7 @@ export default {
 .bookmarksCard {
   padding: 0px;
 }
-.el-button [class*=el-icon-]+span {
-    margin-left: 5px;
+.el-button--small > span {
+  margin-left: 5px;
 }
 </style>
