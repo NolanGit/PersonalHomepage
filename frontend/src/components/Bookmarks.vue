5c5b5afa21589645dc1cd847cf36f011ab7fe470
NolanSun <shr1213@live.com>
2019-11-05 20:45:50 +0800

    20191105

--- frontend/src/components/Bookmarks.vue
+++ frontend/src/components/Bookmarks.vue
@@ -43,7 +43,62 @@
         circle
       ></el-button>
     </el-row>
-
+    <div>
+      <h3 class="message">{{ msg }}</h3>
+      <hr />
+      <h4>Demo1 : list in Y-axis</h4>
+      <SlickList :lockToContainerEdges="true" class="list" lockAxis="y" v-model="list">
+        <SlickItem
+          class="list-item"
+          v-for="(item, index) in list"
+          :index="index"
+          :key="index"
+        >{{ item.title }}</SlickItem>
+      </SlickList>
+      <hr />
+      <h4>Demo2 : parent item in X-axis , children item in Y-ais</h4>
+      <SlickList
+        :lockToContainerEdges="true"
+        axis="x"
+        lockAxis="x"
+        v-model="items"
+        class="SortableList"
+        @input="getChangeLists"
+      >
+        <SlickItem v-for="(item, index) in items" class="SortableItem" :index="index" :key="index">
+          {{ item.name }}
+          <SlickList :lockToContainerEdges="true" class="list" lockAxis="y" v-model="item.itemArr">
+            <SlickItem
+              class="list-item"
+              v-for="(item, index) in item.itemArr"
+              :index="index"
+              :key="index"
+            >{{ item }}</SlickItem>
+          </SlickList>
+        </SlickItem>
+      </SlickList>
+      <hr />
+      <h4>Demo3 : parent item in Y-axis , children item in Y-ais</h4>
+      <SlickList
+        :lockToContainerEdges="true"
+        lockAxis="y"
+        v-model="items"
+        class="SortableList2"
+        @input="getChangeLists"
+      >
+        <SlickItem v-for="(item, index) in items" class="SortableItem2" :index="index" :key="index">
+          <p>{{ item.name }}</p>
+          <SlickList :lockToContainerEdges="true" class="list" lockAxis="y" v-model="item.itemArr">
+            <SlickItem
+              class="list-item"
+              v-for="(item, index) in item.itemArr"
+              :index="index"
+              :key="index"
+            >{{ item }}</SlickItem>
+          </SlickList>
+        </SlickItem>
+      </SlickList>
+    </div>
     <!--编辑界面-->
     <el-dialog
       title="编辑书签"
@@ -51,8 +106,13 @@
       :close-on-click-modal="false"
       width="40%"
     >
-      <SlickList lockAxis="y" v-model="items" class="list">
-        <SlickItem class="list-item" v-for="(item, index) in items" :index="index" :key="index">
+      <SlickList lockAxis="y" v-model="bookmarksEdit.list" class="list">
+        <SlickItem
+          class="list-item"
+          v-for="(item, index) in bookmarksEdit.list"
+          :index="index"
+          :key="index"
+        >
           <span>{{ item }}</span>
         </SlickItem>
       </SlickList>
@@ -83,15 +143,38 @@ export default {
   },
   data() {
     return {
+      msg: "Hello, This is Vue-Slicksort-Demo ！",
+      flag: true,
+      list: [
+        {
+          title: "list1"
+        },
+        {
+          title: "list2"
+        },
+        {
+          title: "list3"
+        },
+        {
+          title: "list4"
+        },
+        {
+          title: "list5"
+        }
+      ],
       items: [
-        "Item 1",
-        "Item 2",
-        "Item 3",
-        "Item 4",
-        "Item 5",
-        "Item 6",
-        "Item 7",
-        "Item 8"
+        {
+          name: "title1",
+          itemArr: ["Item1", "Item2", "Item3"]
+        },
+        {
+          name: "title2",
+          itemArr: ["Item4", "Item5", "Item6"]
+        },
+        {
+          name: "title3",
+          itemArr: ["Item7", "Item8", "Item9"]
+        }
       ],
       bookmarksDataArray: [],
       bookmarksEdit: {
@@ -186,25 +269,72 @@ export default {
   margin-right: 5px;
 }
 .list {
-    max-height: 80vh;
-    margin: 0 auto;
-    padding: 0;
-    overflow: auto;
-    background-color: #f3f3f3;
-    border: 1px solid #efefef;
-    max-width:600px;
-    cursor:pointer;
+  max-height: 80vh;
+  margin: 0 auto;
+  padding: 0;
+  overflow: auto;
+  background-color: #f3f3f3;
+  border: 1px solid #efefef;
+  max-width: 600px;
+  cursor: pointer;
 }
 .list-item {
-    display: flex;
-    align-items: center;
-    width: 100%;
-    padding: 20px;
-    background-color: #fff;
-    border-bottom: 1px solid #efefef;
-    box-sizing: border-box;
-    user-select: none;
-    color: #333;
-    font-weight: 400;
+  display: flex;
+  align-items: center;
+  width: 100%;
+  padding: 20px;
+  background-color: #fff;
+  border-bottom: 1px solid #efefef;
+  box-sizing: border-box;
+  user-select: none;
+  color: #333;
+  font-weight: 400;
+}
+.stylizedHelper {
+  background: blue;
+  color: #fff;
+}
+.SortableList {
+  display: flex;
+  width: 600px;
+  white-space: nowrap;
+  max-height: 80vh;
+  margin: 0 auto;
+  padding: 0;
+  overflow: auto;
+  background-color: #f3f3f3;
+  border: 1px solid #efefef;
+  cursor: pointer;
+}
+.SortableItem {
+  display: flex;
+  align-items: center;
+  width: 100%;
+  padding: 20px;
+  background-color: #fff;
+  border-bottom: 1px solid #efefef;
+  box-sizing: border-box;
+  user-select: none;
+  color: #333;
+  font-weight: 400;
+  border: 1px solid #ccc;
+}
+.SortableList2 {
+  width: 300px;
+  margin: 20px auto;
+  border: 1px solid #efefef;
+}
+.SortableItem2 {
+  width: 300px;
+  border: 1px solid #efefef;
+  text-align: center;
+}
+.SortableItem2 p {
+  font-size: 24px;
+  font-weight: bold;
+  cursor: pointer;
+  background: #efefef;
+  margin: 0;
+  height: 30px;
 }
 </style>
