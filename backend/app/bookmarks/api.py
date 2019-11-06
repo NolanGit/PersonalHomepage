a763f41b1d39280d0626d2407caccc9d7105caf2
NolanSun <shr1213@live.com>
2019-11-06 15:36:50 +0800

    20191106

--- backend/app/bookmarks/api.py
+++ backend/app/bookmarks/api.py
@@ -40,10 +40,42 @@ def bookmarksAdd():
         icon = request.get_json()['icon']
         bookmarks_query = bookmarks_table.select().where((bookmarks_table.user_id == user_id) & (bookmarks_table.is_valid == 1)).order_by(bookmarks_table.order).dicts()
         order = bookmarks_query[-1]['order']
-        bookmarks_query = bookmarks_table.create(name=name, url=url, icon=icon, order=order + 1, user_id=user_id, is_valid=1, update_time=datetime.datetime.now())
+        bookmarks_table.create(name=name, url=url, icon=icon, order=order + 1, user_id=user_id, is_valid=1, update_time=datetime.datetime.now())
         response = {'code': 200, 'msg': '成功！', 'data': result}
-        return jsonify(response)
 
     except Exception as e:
         response = {'code': 500, 'msg': '失败！错误信息：' + str(e) + '，请联系管理员。', 'data': []}
+    finally:
+        return jsonify(response)
+
+
+@bookmarks.route('/bookmarksDelete', methods=['POST'])
+@cross_origin()
+def bookmarksDelete():
+    try:
+        user_id = CommonFunc().get_user_id(request.get_json()['user'])
+        id = request.get_json()['id']
+        bookmarks_table.update(is_valid=0, update_time=datetime.datetime.now()).where(bookmarks_table.id == id).execute()
+        response = {'code': 200, 'msg': '成功！', 'data': []}
+
+    except Exception as e:
+        response = {'code': 500, 'msg': '失败！错误信息：' + str(e) + '，请联系管理员。', 'data': []}
+    finally:
+        return jsonify(response)
+
+
+@bookmarks.route('/bookmarksReorder', methods=['POST'])
+@cross_origin()
+def bookmarksReorder():
+    try:
+        user_id = CommonFunc().get_user_id(request.get_json()['user'])
+        bookmarks = request.get_json()['bookmarks']
+        bookmarks_table.update(is_valid=0, update_time=datetime.datetime.now()).where(bookmarks_table.user_id == user_id).execute()
+        for bookmark in bookmarks:
+            bookmarks_table.create(name=bookmark['name'], url=bookmark['url'], icon=bookmark['icon'], order=bookmark['order'], user_id=user_id, is_valid=1, update_time=datetime.datetime.now())
+        response = {'code': 200, 'msg': '成功！', 'data': []}
+
+    except Exception as e:
+        response = {'code': 500, 'msg': '失败！错误信息：' + str(e) + '，请联系管理员。', 'data': []}
+    finally:
         return jsonify(response)
