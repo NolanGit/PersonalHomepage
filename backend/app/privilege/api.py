cd5781b8ef05f54ee0ebb27476cd8f1bff39025e
NolanSun <shr1213@live.com>
2020-01-14 20:10:31 +0800

    20200114

--- backend/app/privilege/api.py
+++ backend/app/privilege/api.py
@@ -12,6 +12,7 @@ from flask import session, redirect, url_for, current_app, flash, Response, requ
 from .model import role, privilege_role
 from .model import privilege as privilege_model
 from ..common_func import CommonFunc
+from ..login.model import user
 cf = configparser.ConfigParser()
 cf.read('app/homepage.config')
 KEY = cf.get('config', 'KEY')
@@ -22,6 +23,7 @@ def privilege_flush(User):
 
 
 def permission_required(privilege):
+
     def decorator(f):
 
         @wraps(f)
@@ -59,7 +61,8 @@ class privilegeFunction(object):
         self.get_redis_conn().hmset(user_key, user_dict)
         return user_key, privilege_key
 
-    def init_user_and_privilege(self, user_id, user_role_id):
+    def init_user_and_privilege(self, user_id):
+        user_role_id = user.get(id == user_id).role_id
         user_key, privilege_key = self.set_user_to_redis(user_id)
         self.set_user_privilege_to_redis(user_role_id, privilege_key)
         return user_key
