76fd88e666dedfe4880a6217b8a08c624f929b3a
NolanSun <shr1213@live.com>
2020-01-14 19:17:47 +0800

    修改变更字段导致的一个bug

--- backend/app/login/api.py
+++ backend/app/login/api.py
@@ -25,7 +25,7 @@ class User(object):
         user_query = user.select().where(user.name == self.user_name).limit(1).dicts()
         for row in user_query:
             self.user_id = row['id']
-            self.role = row['role']
+            self.role_id = row['role_id']
             self.create_time = row['create_time']
             self.update_time = row['update_time']
 
