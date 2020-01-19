6730679ed7b99a1ff47947b6e0d01ae245695f94
NolanSun <shr1213@live.com>
2020-01-19 21:01:18 +0800

    20200119

--- backend/app/login/model.py
+++ backend/app/login/model.py
@@ -24,6 +24,7 @@ class user(BaseModel):
     salt = CharField()
     salt_expire_time = DateTimeField()
     role_id = IntegerField()
+    is_valid = IntegerField()
     create_time = DateTimeField()
     update_time = DateTimeField()
 
