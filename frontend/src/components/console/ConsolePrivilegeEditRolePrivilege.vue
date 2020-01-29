7dbd87935436df402d6b96beab1167a12743d237
NolanSun <shr1213@live.com>
2020-01-29 09:19:36 +0800

    bug fixed

--- frontend/src/components/console/ConsolePrivilegeEditRolePrivilege.vue
+++ frontend/src/components/console/ConsolePrivilegeEditRolePrivilege.vue
@@ -18,7 +18,7 @@
 
 <script>
 import axios from "axios";
-import rolePrivilegeEdit from "../../api/console";
+import {rolePrivilegeEdit} from "../../api/console";
 export default {
   name: "ConsolePrivilegeEditRolePrivilege",
   props: {
@@ -56,6 +56,7 @@ export default {
         role_id: this.roleId,
         checked_privilege_id: this.checkedPrivilegeId
       };
+      //报错void 0 is not a function
       rolePrivilegeEdit(para).then(data => {
         if (data["code"] !== 200) {
           this.$message({
