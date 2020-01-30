2a3edbcb639e537e14c891f14c5ff06fbdecf1cb
NolanSun <shr1213@live.com>
2020-01-30 19:34:53 +0800

    bug fixed

--- backend/app/main/api.py
+++ backend/app/main/api.py
@@ -7,10 +7,10 @@ import traceback
 from flask import render_template, session, redirect, url_for, current_app, request, jsonify, Response
 from . import main
 from flask_cors import cross_origin
-from ..search.model import search_engines, search_engines_log
-from ..weather.model import weather_personalized
-from ..bookmarks.model import bookmarks as bookmarks_table
-from ..bookmarks.model import icon as icon_table
+from ..model.search_model import search_engines, search_engines_log
+from ..model.weather_model import weather_personalized
+from ..model.bookmarks_model import bookmarks as bookmarks_table
+from ..model.bookmarks_model import icon as icon_table
 from ..login.api import User
 from ..privilege.privilege_control import permission_required
 
