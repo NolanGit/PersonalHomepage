2d0e59d8e1ef6099c7791684e5ae3eb2bf154376
NolanSun <shr1213@live.com>
2020-02-18 01:29:57 +0800

    20200218

--- frontend/src/api/console.js
+++ frontend/src/api/console.js
@@ -2,6 +2,8 @@ import axios from 'axios'
 
 let base = ''
 
+export const consoleGet = params => { return axios.get(`${base}/console/get`, params).then(res => res.data) }
+
 export const consoleScriptRun = params => { return axios.post(`${base}/script/run`, params).then(res => res.data) }
 
 export const consoleScriptRunOutput = params => { return axios.post(`${base}/script/runOutput`, params).then(res => res.data) }
