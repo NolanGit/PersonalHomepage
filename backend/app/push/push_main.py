45790da1c65fc823ca474132ed021c7b834db3a8
NolanSun <shr1213@live.com>
2020-02-25 23:13:59 +0800

    20200225

--- backend/app/push/push_main.py
+++ backend/app/push/push_main.py
@@ -1,1 +1,13 @@
+import datetime
 from ..model.push_model import push as push_table
+
+
+def push():
+    push_list = push_table.select().where(push_table.status == 0).dicts()
+    for push in push_list:
+        if push['trigger_time'] <= datetime.datetime.now():
+            pass
+            #user_id
+            #method
+            #address
+            #content
