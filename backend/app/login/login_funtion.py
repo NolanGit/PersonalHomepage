6168efd4258159eedf5ffc4a12a5ffc04b2a1274
NolanSun <shr1213@live.com>
2020-02-26 22:11:20 +0800

    20200226

--- backend/app/login/login_funtion.py
+++ backend/app/login/login_funtion.py
@@ -1,5 +1,10 @@
-from ..model.login_model import user
-
+try:
+    from ..model.login_model import user
+except:
+    import sys
+    sys.path.append('../')
+    sys.path.append('../../')
+    from model.login_model import user
 
 class User(object):
 
