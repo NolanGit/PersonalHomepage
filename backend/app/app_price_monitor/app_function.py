c093d18e41c08c111f55ace8f3845d5b4d67504a
NolanSun <shr1213@live.com>
2020-02-26 22:20:19 +0800

    bug fixed

--- backend/app/app_price_monitor/app_function.py
+++ backend/app/app_price_monitor/app_function.py
@@ -11,6 +11,7 @@ except:
     from model.app_model import app_price
     from model.app_model import app_push
 
+
 def app_get(user_id=0):
     '''
         获取用户id下有效的app，user_id不传则获取全部
@@ -22,7 +23,7 @@ def app_get(user_id=0):
         app_table_query = app_table.select().where(app_table.is_valid == 1).dicts()
     else:
         app_table_query = app_table.select().where((app_table.user_id == user_id) & (app_table.is_valid == 1)).order_by(app_table.order).dicts()
-    result = [{row['id'], row['name'], row['except_price'], row['update_time']} for row in app_table_query]
+    result = [{'id': row['id'], 'name': row['name'], 'except_price': row['except_price'], 'update_time': row['update_time']} for row in app_table_query]
     return result
 
 
@@ -54,8 +55,14 @@ def app_push_get():
     '''
     app_push_valids = app_push.select().where((app_push.is_valid == 1) & (app_push.notify == 1) & (app_push.notify_trigger_time <= datetime.datetime.now())).dicts()
     return [{
-        app_push_valid['id'], app_push_valid['user_id'], app_push_valid['notify'], app_push_valid['notify_method'], app_push_valid['notify_interval_raw'], app_push_valid['notify_interval_unit'],
-        app_push_valid['notify_interval'], app_push_valid['notify_trigger_time']
+        'id': app_push_valid['id'],
+        'user_id': app_push_valid['user_id'],
+        'notify': app_push_valid['notify'],
+        'notify_method': app_push_valid['notify_method'],
+        'notify_interval_raw': app_push_valid['notify_interval_raw'],
+        'notify_interval_unit': app_push_valid['notify_interval_unit'],
+        'notify_interval': app_push_valid['notify_interval'],
+        'notify_trigger_time': app_push_valid['notify_trigger_time']
     } for app_push_valid in app_push_valids]
 
 
