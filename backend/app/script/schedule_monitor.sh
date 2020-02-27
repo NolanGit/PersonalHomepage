c94684761ccdf7c19d37182de31de26a32b7b8af
NolanSun <shr1213@live.com>
2020-02-27 10:26:05 +0800

    20200227

--- backend/app/script/schedule_monitor.sh
+++ backend/app/script/schedule_monitor.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-cd /home/pi/Documents/Github/PersonalHomepage/backend/app/console
+cd /home/pi/Documents/Github/PersonalHomepage/backend/app/script
 /usr/bin/python3.5 schedule_monitor.py
