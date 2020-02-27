787ab9b48cc4ba085cb503e8f93a5a9d3a2b7255
NolanSun <shr1213@live.com>
2020-02-27 20:17:14 +0800

    20200227

--- backend/app/push/push_main.py
+++ backend/app/push/push_main.py
@@ -1,4 +1,5 @@
 import datetime
+import traceback
 try:
     from ..model.push_model import push as push_table
     from ..tool.wechat_sender import Wechat
@@ -17,23 +18,28 @@ except:
 
 def push():
     push_list = push_table.select().where(push_table.status == 0).dicts()
-    for push in push_list:
-        if push['trigger_time'] <= datetime.datetime.now():
-            id = push['id']
-            push_table.update(status=1).where(push_table.id == id).execute()
-            method = push['method']
-            address = push['address']
-            title = push['title']
-            content = push['content']
-            print('推送:[%s]%s' % (title, content))
-            if method == 1:  # 微信
-                log = Wechat(title, content, address).send()
-            elif method == 2:  # 邮件
-                log = Mail('推送通知', title, content, address).send()
-            if log['code'] == 200:
-                push_table.update(status=2, log=str(log)).where(push_table.id == id).execute()
-            else:
-                push_table.update(status=0, log=str(log)).where(push_table.id == id).execute()
+    print(push_list)
+    print(type(push_list))
+    try:
+        for push in push_list:
+            if push['trigger_time'] <= datetime.datetime.now():
+                id = push['id']
+                push_table.update(status=1).where(push_table.id == id).execute()
+                method = push['method']
+                address = push['address']
+                title = push['title']
+                content = push['content']
+                print('推送:[%s]%s' % (title, content))
+                if method == 1:  # 微信
+                    log = Wechat(title, content, address).send()
+                elif method == 2:  # 邮件
+                    log = Mail('推送通知', title, content, address).send()
+                if log['code'] == 200:
+                    push_table.update(status=2, log=str(log)).where(push_table.id == id).execute()
+                else:
+                    push_table.update(status=0, log=str(log)).where(push_table.id == id).execute()
+    except:
+        traceback.print_exc()
 
 
 push()
