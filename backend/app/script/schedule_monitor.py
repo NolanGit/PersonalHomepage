5e5535a0deb3f8734a77c7abe4f4f4254f8ec442
NolanSun <shr1213@live.com>
2020-02-27 12:17:05 +0800

    20200227

--- backend/app/script/schedule_monitor.py
+++ backend/app/script/schedule_monitor.py
@@ -22,37 +22,39 @@ def run(schedules):
     try:
         for schedule in schedules:
             if schedule['is_valid'] == 1:
-                script_schedule.update(is_valid=2).where(script_schedule.id == schedule['id']).execute()
+                try:
+                    script_schedule.update(is_valid=2).where(script_schedule.id == schedule['id']).execute()
 
-                #记录运行次数
-                script_id = schedule['script_id']
-                script_query = script.select().where((script.is_valid == 1) & (script.id == script_id)).dicts()
-                for row in script_query:
-                    runs = int(row['runs']) + 1
-                script_query = script(id=script_id)
-                script_query.runs = int(runs)
-                script_query.save()
+                    #记录运行次数
+                    script_id = schedule['script_id']
+                    script_query = script.select().where((script.is_valid == 1) & (script.id == script_id)).dicts()
+                    for row in script_query:
+                        runs = int(row['runs']) + 1
+                    script_query = script(id=script_id)
+                    script_query.runs = int(runs)
+                    script_query.save()
 
-                start_time = datetime.datetime.now()
-                subprocess_instance = subprocess.Popen(schedule['command'], shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, bufsize=1)
-                output = ''
-                for x in range(3000):
-                    temp_output = subprocess_instance.stdout.readline()
-                    output = output + str(temp_output.decode('utf-8'))
-                    if subprocess_instance.poll() != None:
-                        break
-                print(output)
-                script_log.create(
-                    script_id=schedule['script_id'],
-                    command=schedule['command'],
-                    detail=schedule['detail'],
-                    version=schedule['version'],
-                    output=schedule['command'] + '<br>' + str(output).replace('\n', '<br>').replace(' ', '&nbsp;'),
-                    user=schedule['user'] + '(定时)',
-                    start_time=start_time,
-                    end_time=datetime.datetime.now())
-                script_schedule.update(is_valid=0).where(script_schedule.id == schedule['id']).execute()
-                generate_next_schedule(schedule)
+                    start_time = datetime.datetime.now()
+                    subprocess_instance = subprocess.Popen(schedule['command'], shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, bufsize=1)
+                    output = ''
+                    for x in range(3000):
+                        temp_output = subprocess_instance.stdout.readline()
+                        output = output + str(temp_output.decode('utf-8'))
+                        if subprocess_instance.poll() != None:
+                            break
+                    print(output)
+                    script_log.create(script_id=schedule['script_id'],
+                                      command=schedule['command'],
+                                      detail=schedule['detail'],
+                                      version=schedule['version'],
+                                      output=schedule['command'] + '<br>' + str(output).replace('\n', '<br>').replace(' ', '&nbsp;'),
+                                      user=schedule['user'] + '(定时)',
+                                      start_time=start_time,
+                                      end_time=datetime.datetime.now())
+                    script_schedule.update(is_valid=0).where(script_schedule.id == schedule['id']).execute()
+                    generate_next_schedule(schedule)
+                except:
+                    script_schedule.update(is_valid=2).where(script_schedule.id == schedule['id']).execute()
     except Exception as e:
         print(e)
         traceback.print_exc()
@@ -60,19 +62,18 @@ def run(schedules):
 
 def generate_next_schedule(schedule):
     if schedule['is_automatic'] == 1:
-        script_schedule.create(
-            script_id=schedule['script_id'],
-            command=schedule['command'],
-            detail=str(schedule['detail']),
-            version=schedule['version'],
-            user=schedule['user'],
-            is_automatic=schedule['is_automatic'],
-            trigger_time=schedule['trigger_time'] + datetime.timedelta(hours=schedule['interval']),
-            interval=schedule['interval'],
-            interval_raw=schedule['interval_raw'],
-            interval_unit=schedule['interval_unit'],
-            is_valid=1,
-            update_time=schedule['update_time'])
+        script_schedule.create(script_id=schedule['script_id'],
+                               command=schedule['command'],
+                               detail=str(schedule['detail']),
+                               version=schedule['version'],
+                               user=schedule['user'],
+                               is_automatic=schedule['is_automatic'],
+                               trigger_time=schedule['trigger_time'] + datetime.timedelta(hours=schedule['interval']),
+                               interval=schedule['interval'],
+                               interval_raw=schedule['interval_raw'],
+                               interval_unit=schedule['interval_unit'],
+                               is_valid=1,
+                               update_time=schedule['update_time'])
 
 
 run(schedule_get())
