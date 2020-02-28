94213ec451c53add2bf64bff788aaea28129b702
NolanSun <shr1213@live.com>
2020-02-28 10:38:32 +0800

    20200228

--- backend/app/bookmarks/api.py
+++ backend/app/bookmarks/api.py
@@ -34,27 +34,27 @@ from ..login.login_funtion import User
 #         return jsonify(response), 500
 
 
-# @bookmarks.route('/bookmarksAdd', methods=['POST'])
-# @cross_origin()
-# def bookmarksAdd():
-#     try:
-#         try:
-#             user_name = request.get_json()['user']
-#             user = User(user_name)
-#             user_id = user.user_id
-#         except:
-#             user_id = 0
-#         name = request.get_json()['name']
-#         url = request.get_json()['url']
-#         icon = request.get_json()['icon']
-#         bookmarks_query = bookmarks_table.select().where((bookmarks_table.user_id == user_id) & (bookmarks_table.is_valid == 1)).order_by(bookmarks_table.order).dicts()
-#         order = bookmarks_query[-1]['order'] + 1
-#         bookmarks_table.create(name=name, url=url, icon=icon, order=order, user_id=user_id, is_valid=1, update_time=datetime.datetime.now())
-#         response = {'code': 200, 'msg': '成功！'}
-#         return jsonify(response)
-#     except Exception as e:
-#         response = {'code': 500, 'msg': '失败！错误信息：' + str(e) + '，请联系管理员。', 'data': []}
-#         return jsonify(response), 500
+@bookmarks.route('/bookmarksAdd', methods=['POST'])
+@cross_origin()
+def bookmarksAdd():
+    try:
+        try:
+            user_name = request.get_json()['user']
+            user = User(user_name)
+            user_id = user.user_id
+        except:
+            user_id = 0
+        name = request.get_json()['name']
+        url = request.get_json()['url']
+        icon = request.get_json()['icon']
+        bookmarks_query = bookmarks_table.select().where((bookmarks_table.user_id == user_id) & (bookmarks_table.is_valid == 1)).order_by(bookmarks_table.order).dicts()
+        order = bookmarks_query[-1]['order'] + 1
+        bookmarks_table.create(name=name, url=url, icon=icon, order=order, user_id=user_id, is_valid=1, update_time=datetime.datetime.now())
+        response = {'code': 200, 'msg': '成功！'}
+        return jsonify(response)
+    except Exception as e:
+        response = {'code': 500, 'msg': '失败！错误信息：' + str(e) + '，请联系管理员。', 'data': []}
+        return jsonify(response), 500
 
 
 @bookmarks.route('/bookmarksDelete', methods=['POST'])
