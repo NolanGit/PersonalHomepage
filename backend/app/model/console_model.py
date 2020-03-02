74bbb48afb1c22795f5226873e76be436aed2776
NolanSun <shr1213@live.com>
2020-03-02 23:17:27 +0800

    20200302

--- backend/app/model/console_model.py
+++ backend/app/model/console_model.py
@@ -2,20 +2,25 @@ import os
 import peewee
 import configparser
 from peewee import *
+from playhouse.pool import PooledMySQLDatabase
 
+PATH = lambda p: os.path.abspath(os.path.join(os.path.dirname(__file__), p))
 cf = configparser.ConfigParser()
-cf.read('../homepage.config')
+cf.read(PATH('../homepage.config'))
 DB_PASS = cf.get('config', 'DB_PASS')
 
 
 class UnknownField(object):
+
     def __init__(self, *_, **__):
         pass
 
 
 class BaseModel(Model):
+
     class Meta:
-        database = MySQLDatabase('my_app', user='root', password=DB_PASS, host='localhost', port=3306)
+        database = PooledMySQLDatabase('my_app', user='root', password=DB_PASS, host='localhost', port=3306)
+
 
 
 class console_script_sub_system(BaseModel):
