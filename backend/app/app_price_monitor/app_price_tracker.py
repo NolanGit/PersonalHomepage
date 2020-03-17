341ff15583f7fb5a9cbbdbadebe3fb98450244ad
NolanSun <shr1213@live.com>
2020-03-17 20:39:21 +0800

    20200317

--- backend/app/app_price_monitor/app_price_tracker.py
+++ backend/app/app_price_monitor/app_price_tracker.py
@@ -17,24 +17,23 @@ def app_price_push_generator():
         首先获取所有需要推送的用户id，然后取该用户id下的app（id，名称、期望价格），然后去价格表查最新的一条
     '''
 
-    app_push_list = PushList(APP_WIDGET_ID).push_list_get().push_list
-    for app_push_data in app_push_list:
-        push_data = PushData(app_push_data)
-        user_id = app_push_data['user_id']
+    app_push_data_list = PushList(APP_WIDGET_ID).push_list_get().push_list
+    for app_push_data in app_push_data_list:
+        user_id = app_push_data.user_id
 
         content = ''
-        applist = app_get(push_data.user_id)
+        applist = app_get(app_push_data.user_id)
         for app in applist:
             current_price, update_time = app_price_get(app['id'])
             if float(current_price) <= float(app['expect_price']):
                 content = content + '\n' + '[' + app['name'] + ']' + ' is ¥' + str(current_price) + ' now !(' + update_time + ')' + '\n'
         if content != '':
-            if push_data.notify_method == 1:
+            if app_push_data.notify_method == 1:
                 address = User(user_id=user_id).wechat_key
-            elif push_data.notify_method == 2:
+            elif app_push_data.notify_method == 2:
                 address = User(user_id=user_id).email
             title = 'App Discount!'
-            if (push_data.add_to_push_queue(title, address, content)):
-                if (push_data.generate_next()):
-                    push_data.delete()
+            if (app_push_data.add_to_push_queue(title, address, content)):
+                if (app_push_data.generate_next()):
+                    app_push_data.delete()
 
--- backend/app/push/push_function.py
+++ backend/app/push/push_function.py
@@ -13,7 +13,21 @@ except:
 class PushData(object):
 
     def __init__(self, param_dict):
-        self.id = param_dict['id']
+        '''
+            args:
+                param_dict:
+                    id(optional):Int
+                    user_id
+                    widget_id
+                    notify
+                    notify_method
+                    notify_interval_raw
+                    notify_interval_unit
+                    notify_interval
+                    notify_trigger_time
+                    update_time
+        '''
+        self.id = param_dict['id'] if 'id' in param_dict else 0
         self.user_id = param_dict['user_id']
         self.widget_id = param_dict['widget_id']
         self.notify = param_dict['notify']
@@ -23,6 +37,24 @@ class PushData(object):
         self.notify_interval = param_dict['notify_interval']
         self.notify_trigger_time = param_dict['notify_trigger_time']
         self.update_time = param_dict['update_time']
+
+    def save(self):
+        try:
+            push.create(
+                user_id=self.user_id,
+                widget_id=self.widget_id,
+                is_valid=1,
+                notify=self.notify,
+                notify_method=self.notify_method,
+                notify_interval_raw=self.notify_interval_raw,
+                notify_interval_unit=self.notify_interval_unit,
+                notify_interval=self.notify_interval,
+                notify_trigger_time=self.notify_trigger_time,
+                update_time=datetime.datetime.now())
+            return True
+        except Exception as e:
+            print('保存推送记录失败' + str(e))
+            return False
 
     def delete(self):
         '''
@@ -47,7 +79,7 @@ class PushData(object):
                 update_time=datetime.datetime.now())
             return True
         except Exception as e:
-            print('加入推送队列失败:'+str(e))
+            print('加入推送队列失败:' + str(e))
             return False
 
     def generate_next(self):
@@ -68,7 +100,7 @@ class PushData(object):
                 update_time=datetime.datetime.now())
             return True
         except Exception as e:
-            print('生成下一条待推送记录失败'+str(e))
+            print('生成下一条待推送记录失败' + str(e))
             return False
 
 
@@ -82,21 +114,23 @@ class PushList(object):
             返回生效状态且需要推送且已到推送时间的推送列表
 
             returns: self
-            affects: self.push_list(List)[{id, user_id, widget_id, notify, notify_method, notify_interval_raw, notify_interval_unit, notify_interval, notify_trigger_time}...]
+            affects: self.push_list(List)[PushData...]
         '''
         push_valids = push.select().where((push.widget_id == self.widget_id) & (push.is_valid == 1) & (push.notify == 1) & (push.notify_trigger_time <= datetime.datetime.now())).dicts()
-        self.push_list = [{
-            'id': push_valid['id'],
-            'user_id': push_valid['user_id'],
-            'widget_id': push_valid['widget_id'],
-            'notify': push_valid['notify'],
-            'notify_method': push_valid['notify_method'],
-            'notify_interval_raw': push_valid['notify_interval_raw'],
-            'notify_interval_unit': push_valid['notify_interval_unit'],
-            'notify_interval': push_valid['notify_interval'],
-            'notify_trigger_time': push_valid['notify_trigger_time'],
-            'update_time': push_valid['update_time']
-        } for push_valid in push_valids]
+        self.push_list = [
+            PushData({
+                'id': push_valid['id'],
+                'user_id': push_valid['user_id'],
+                'widget_id': push_valid['widget_id'],
+                'notify': push_valid['notify'],
+                'notify_method': push_valid['notify_method'],
+                'notify_interval_raw': push_valid['notify_interval_raw'],
+                'notify_interval_unit': push_valid['notify_interval_unit'],
+                'notify_interval': push_valid['notify_interval'],
+                'notify_trigger_time': push_valid['notify_trigger_time'],
+                'update_time': push_valid['update_time']
+            }) for push_valid in push_valids
+        ]
         return self
 
 
