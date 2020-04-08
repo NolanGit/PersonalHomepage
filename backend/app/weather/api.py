773b24ca06916ebcfd6cb5896922ccc3a7a14b6f
NolanSun <shr1213@live.com>
2020-04-08 20:38:02 +0800

    20200408

--- backend/app/weather/api.py
+++ backend/app/weather/api.py
@@ -10,6 +10,7 @@ from flask import render_template, session, redirect, url_for, current_app, flas
 from ..model.weather_model import weather_personalized
 from ..login.login_funtion import User
 from ..privilege.privilege_control import permission_required
+from .weather_function import WeatherData, WeatherLocation, WeatherLocationList
 
 cf = configparser.ConfigParser()
 cf.read('app/homepage.config')
@@ -17,29 +18,29 @@ KEY = cf.get('config', 'KEY')
 
 URL_PREFIX = 'weather'
 
-@weather.route('/get', methods=['POST'])
-#@permission_required(URL_PREFIX + '/get')
-@cross_origin()
-def userInfo():
-    try:
-        result = {}
-        try:
-            user_id = request.get_json()['user_id']
-        except:
-            user_id = 0
+# @weather.route('/get', methods=['POST'])
+# #@permission_required(URL_PREFIX + '/get')
+# @cross_origin()
+# def userInfo():
+#     try:
+#         result = {}
+#         try:
+#             user_id = request.get_json()['user_id']
+#         except:
+#             user_id = 0
 
-        if user_id != 0:
-            weather_personalized_query = weather_personalized.select().where((weather_personalized.user_id == user_id) & (weather_personalized.is_valid == 1)).dicts()
-            result['locations'] = []
-            for row in weather_personalized_query:
-                result['locations'].append(row['location'])
+#         if user_id != 0:
+#             weather_personalized_query = weather_personalized.select().where((weather_personalized.user_id == user_id) & (weather_personalized.is_valid == 1)).dicts()
+#             result['locations'] = []
+#             for row in weather_personalized_query:
+#                 result['locations'].append(row['location'])
 
-        response = {'code': 200, 'msg': '成功！', 'data': result}
-        return jsonify(response)
-    except Exception as e:
-        traceback.print_exc()
-        response = {'code': 500, 'msg': '失败！错误信息：' + str(e) + '，请联系管理员。', 'data': []}
-        return jsonify(response), 500
+#         response = {'code': 200, 'msg': '成功！', 'data': result}
+#         return jsonify(response)
+#     except Exception as e:
+#         traceback.print_exc()
+#         response = {'code': 500, 'msg': '失败！错误信息：' + str(e) + '，请联系管理员。', 'data': []}
+#         return jsonify(response), 500
 
 
 @weather.route('/weatherData', methods=['POST'])
@@ -94,15 +95,15 @@ def weatherData():
         return jsonify(response), 500
 
 
-@weather.route('/weatherPersonalizedSave', methods=['POST'])
-@cross_origin()
-def weatherPersonalizedSave():
-    try:
-        user_id = request.get_json()['user_id']
-        location = request.get_json()['location']
-        weather_personalized.create(location=location, user_id=user_id, is_valid=1, update_time=datetime.datetime.now())
-        return jsonify({'code': 200, 'msg': '成功！', 'data': []})
-    except Exception as e:
-        traceback.print_exc()
-        response = {'code': 500, 'msg': '失败！错误信息：' + str(e) + '，请联系管理员。', 'data': []}
-        return jsonify(response), 500
+# @weather.route('/weatherPersonalizedSave', methods=['POST'])
+# @cross_origin()
+# def weatherPersonalizedSave():
+#     try:
+#         user_id = request.get_json()['user_id']
+#         location = request.get_json()['location']
+#         weather_personalized.create(location=location, user_id=user_id, is_valid=1, update_time=datetime.datetime.now())
+#         return jsonify({'code': 200, 'msg': '成功！', 'data': []})
+#     except Exception as e:
+#         traceback.print_exc()
+#         response = {'code': 500, 'msg': '失败！错误信息：' + str(e) + '，请联系管理员。', 'data': []}
+#         return jsonify(response), 500
--- backend/app/weather/weather_function.py
+++ backend/app/weather/weather_function.py
@@ -12,12 +12,139 @@ except:
     from model.weather_model import weather_data
 
 
+class WeatherData(object):
+    def __init__(self, location_id, location):
+        '''
+            location_id
+            location
+        '''
+        self.location_id = location_id
+        self.location = location
+
+    def get_weather_data_from_api(self):
+        import requests
+        import configparser
+
+        cf = configparser.ConfigParser()
+        cf.read('app/homepage.config')
+        KEY = cf.get('config', 'KEY')
+
+        try:
+            result = {}
+            url = 'https://free-api.heweather.net/s6/weather'
+            payload = {'location': self.location, 'key': KEY}
+            r = requests.post(url, params=payload)
+            request_result = r.json()
+
+            result['fl'] = None
+            try:
+                result['fl'] = request_result['HeWeather6'][0]['now']['fl']
+            except:
+                result['fl'] = '暂无数据'
+            result['tmp'] = request_result['HeWeather6'][0]['now']['tmp']
+            result['wind'] = request_result['HeWeather6'][0]['now']['wind_dir'] + str(request_result['HeWeather6'][0]['now']['wind_sc']) + '级'
+            result['cond_code_d'] = request_result['HeWeather6'][0]['daily_forecast'][0]['cond_code_d']
+            result['cond_txt_d'] = request_result['HeWeather6'][0]['daily_forecast'][0]['cond_txt_d']
+            result['cond_code_n'] = request_result['HeWeather6'][0]['daily_forecast'][0]['cond_code_n']
+            result['cond_txt_n'] = request_result['HeWeather6'][0]['daily_forecast'][0]['cond_txt_n']
+            result['tmp_max'] = request_result['HeWeather6'][0]['daily_forecast'][0]['tmp_max']
+            result['tmp_min'] = request_result['HeWeather6'][0]['daily_forecast'][0]['tmp_min']
+            result['tomorrow_cond_code_d'] = request_result['HeWeather6'][0]['daily_forecast'][1]['cond_code_d']
+            result['tomorrow_cond_txt_d'] = request_result['HeWeather6'][0]['daily_forecast'][1]['cond_txt_d']
+            result['tomorrow_tmp_max'] = request_result['HeWeather6'][0]['daily_forecast'][1]['tmp_max']
+            result['tomorrow_tmp_min'] = request_result['HeWeather6'][0]['daily_forecast'][1]['tmp_min']
+            result['r'] = requests.get('https://free-api.heweather.net/s6/air/now', params=payload)
+            result['aqi'] = None
+            try:
+                result['aqi'] = r.json()['HeWeather6'][0]['air_now_city']['aqi']
+            except:
+                result['aqi'] = '暂无数据'
+            return result
+        except Exception as e:
+            traceback.print_exc()
+            print(e)
+            return {}
+
+    def get_latest(self):
+        weather_data_query = weather_data.select().where(weather_data.location_id == self.location_id).order_by(-weather_data.update_time).limit(1).dicts()[0]
+        if len(weather_data_query) != 0:
+            self.weather_data_id = weather_data_query['weather_data_id']
+            self.aqi = weather_data_query['aqi']
+            self.cond_code_d = weather_data_query['cond_code_d']
+            self.cond_code_n = weather_data_query['cond_code_n']
+            self.cond_txt_d = weather_data_query['cond_txt_d']
+            self.cond_txt_n = weather_data_query['cond_txt_n']
+            self.fl = weather_data_query['fl']
+            self.tmp = weather_data_query['tmp']
+            self.tmp_max = weather_data_query['tmp_max']
+            self.tmp_min = weather_data_query['tmp_min']
+            self.tomorrow_cond_code_d = weather_data_query['tomorrow_cond_code_d']
+            self.tomorrow_cond_txt_d = weather_data_query['tomorrow_cond_txt_d']
+            self.tomorrow_tmp_max = weather_data_query['tomorrow_tmp_max']
+            self.tomorrow_tmp_min = weather_data_query['tomorrow_tmp_min']
+            self.wind = weather_data_query['wind']
+            self.update_time = weather_data_query['update_time']
+            return True
+        else:
+            return False
+
+    def update_self(self):
+        result = self.get_weather_data_from_api()
+
+        try:
+            self.fl = result['fl']
+            self.tmp = result['tmp']
+            self.wind = result['wind']
+            self.cond_code_d = result['cond_code_d']
+            self.cond_txt_d = result['cond_txt_d']
+            self.cond_code_n = result['cond_code_n']
+            self.cond_txt_n = result['cond_txt_n']
+            self.tmp_max = result['tmp_max']
+            self.tmp_min = result['tmp_min']
+            self.tomorrow_cond_code_d = result['tomorrow_cond_code_d']
+            self.tomorrow_cond_txt_d = result['tomorrow_cond_txt_d']
+            self.tomorrow_tmp_max = result['tomorrow_tmp_max']
+            self.tomorrow_tmp_min = result['tomorrow_tmp_min']
+            self.aqi = result['aqi']
+            return True
+
+        except Exception as e:
+            traceback.print_exc()
+            print(e)
+            return False
+
+    def save(self):
+        try:
+            weather_data(location_id=self.location_id,
+                         aqi=self.aqi,
+                         cond_code_d=self.cond_code_d,
+                         cond_code_n=self.cond_code_n,
+                         cond_txt_d=self.cond_txt_d,
+                         cond_txt_n=self.cond_txt_n,
+                         fl=self.fl,
+                         tmp=self.tmp,
+                         tmp_max=self.tmp_max,
+                         tmp_min=self.tmp_min,
+                         tomorrow_cond_code_d=self.tomorrow_cond_code_d,
+                         tomorrow_cond_txt_d=self.tomorrow_cond_txt_d,
+                         tomorrow_tmp_max=self.tomorrow_tmp_max,
+                         tomorrow_tmp_min=self.tomorrow_tmp_min,
+                         wind=self.wind,
+                         update_time=datetime.datetime.now()).save()
+            return True
+
+        except Exception as e:
+            traceback.print_exc()
+            print(e)
+            return False
+
+
 class WeatherLocation(object):
     def __init__(
         self,
-        id,
-        location,
-        user_id,
+        id=0,
+        location=None,
+        user_id=0,
     ):
         '''
             id
@@ -27,67 +154,10 @@ class WeatherLocation(object):
         self.id = id
         self.location = location
         self.user_id = user_id
-        
-    def weather_data_get_latest(self):
-        weather_data_query = weather_data.select().where(weather_data.location_id == self.id).order_by(-weather_data.update_time).limit(1).dicts()[0]
-        self.weather_data_id = weather_data_query['weather_data_id']
-        self.aqi = weather_data_query['aqi']
-        self.cond_code_d = weather_data_query['cond_code_d']
-        self.cond_code_n = weather_data_query['cond_code_n']
-        self.cond_txt_d = weather_data_query['cond_txt_d']
-        self.cond_txt_n = weather_data_query['cond_txt_n']
-        self.fl = weather_data_query['fl']
-        self.tmp = weather_data_query['tmp']
-        self.tmp_max = weather_data_query['tmp_max']
-        self.tmp_min = weather_data_query['tmp_min']
-        self.tomorrow_cond_code_d = weather_data_query['tomorrow_cond_code_d']
-        self.tomorrow_cond_txt_d = weather_data_query['tomorrow_cond_txt_d']
-        self.tomorrow_tmp_max = weather_data_query['tomorrow_tmp_max']
-        self.tomorrow_tmp_min = weather_data_query['tomorrow_tmp_min']
-        self.wind = weather_data_query['wind']
-        self.update_time = weather_data_query['update_time']
 
-    def weather_data_update_from_api(self):
-        import requests
-        import configparser
-
-        cf = configparser.ConfigParser()
-        cf.read('app/homepage.config')
-        KEY = cf.get('config', 'KEY')
-        
-        try:
-            url = 'https://free-api.heweather.net/s6/weather'
-            payload = {'location': self.location, 'key': KEY}
-            r = requests.post(url, params=payload)
-            request_result = r.json()
-            try:
-                response['fl'] = request_result['HeWeather6'][0]['now']['fl']
-            except:
-                response['fl'] = '暂无数据'
-            response['tmp'] = request_result['HeWeather6'][0]['now']['tmp']
-            response['wind'] = request_result['HeWeather6'][0]['now']['wind_dir'] + str(request_result['HeWeather6'][0]['now']['wind_sc']) + '级'
-            response['cond_code_d'] = request_result['HeWeather6'][0]['daily_forecast'][0]['cond_code_d']
-            response['cond_txt_d'] = request_result['HeWeather6'][0]['daily_forecast'][0]['cond_txt_d']
-            response['cond_code_n'] = request_result['HeWeather6'][0]['daily_forecast'][0]['cond_code_n']
-            response['cond_txt_n'] = request_result['HeWeather6'][0]['daily_forecast'][0]['cond_txt_n']
-            response['tmp_max'] = request_result['HeWeather6'][0]['daily_forecast'][0]['tmp_max']
-            response['tmp_min'] = request_result['HeWeather6'][0]['daily_forecast'][0]['tmp_min']
-            response['tomorrow_cond_code_d'] = request_result['HeWeather6'][0]['daily_forecast'][1]['cond_code_d']
-            response['tomorrow_cond_txt_d'] = request_result['HeWeather6'][0]['daily_forecast'][1]['cond_txt_d']
-            response['tomorrow_tmp_max'] = request_result['HeWeather6'][0]['daily_forecast'][1]['tmp_max']
-            response['tomorrow_tmp_min'] = request_result['HeWeather6'][0]['daily_forecast'][1]['tmp_min']
-            r = requests.get('https://free-api.heweather.net/s6/air/now', params=payload)
-            try:
-                response['aqi'] = r.json()['HeWeather6'][0]['air_now_city']['aqi']
-            except:
-                response['aqi'] = '暂无数据'
-            
-
-            return jsonify({'code': 200, 'msg': '成功！', 'data': result})
-        except Exception as e:
-            traceback.print_exc()
-            response = {'code': 500, 'msg': '失败！错误信息：' + str(e) + '，请联系管理员。', 'data': []}
-            return jsonify(response), 500
+    def add(self):
+        weather_location(location=self.location, user_id=self.user_id, is_valid=1, update_time=datetime.datetime.now()).save()
+        return self
 
 
 class WeatherLocationList(object):
@@ -110,3 +180,22 @@ class WeatherLocationList(object):
             WeatherLocation(single_weather_location_query['id'], single_weather_location_query['location'], single_weather_location_query['user_id'])
             for single_weather_location_query in weather_location_query
         ]
+        return self
+
+    def delete(self):
+        try:
+            if self.user_id == 0 and self.is_valid != 0:
+                weather_location.update(is_valid=0).where((weather_location.user_id == self.user_id) & (weather_location.is_valid == self.is_valid)).execute()
+            return True
+        except Exception as e:
+            traceback.print_exc()
+            print(e)
+            return False
+
+
+if __name__ == '__main__':
+    weather_location_list = WeatherLocationList().get().list
+    for weather_location in weather_location_list:
+        weather_data = WeatherData(weather_location.id, weather_location.location)
+        weather_data.get_weather_data_from_api()
+        weather_data.save()
