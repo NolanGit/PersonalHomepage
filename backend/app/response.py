91db7ce002a3fc1de982477a0a58659896e59225
NolanSun <shr1213@live.com>
2020-05-10 11:24:09 +0800

    bug fixed

--- backend/app/response.py
+++ backend/app/response.py
@@ -1,6 +1,10 @@
+from flask import jsonify
+
+
 class Response(object):
+
     def success(self, data=[]):
-        return {'code': 200, 'msg': '成功！', 'data': data}
+        return jsonify({'code': 200, 'msg': '成功！', 'data': data})
 
     def failed(self, msg=''):
-        return {'code': 500, 'msg': '失败！错误信息：' + str(msg) + '，请联系管理员。'}
+        return jsonify({'code': 500, 'msg': '失败！错误信息：' + str(msg) + '，请联系管理员。'})
