e9b59c4c4970021d7d44eaf491d05bc1749367a6
NolanSun <shr1213@live.com>
2020-06-27 23:52:42 +0800

    20200627

---
+++ backend/app/model/ip_location_model.py
@@ -0,0 +1,18 @@
+import os
+import peewee
+import configparser
+from peewee import *
+from playhouse.pool import PooledMySQLDatabase
+from .model_function import BaseModel
+
+
+class ip_location(BaseModel):
+    ip = CharField()
+    location = CharField()
+    update_time = DateTimeField()
+
+    class Meta:
+        table_name = 'ip_location'
+
+
+ip_location.create_table()
