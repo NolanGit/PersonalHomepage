2a9ac4c89a2635d515a467873ff741c4aa6a1349
NolanSun <shr1213@live.com>
2020-06-28 21:14:35 +0800

    20200628

---
+++ backend/app/gold_price_monitor/gole_price_spider.py
@@ -0,0 +1,49 @@
+# -*- coding:utf-8 -*-
+import os
+import sys
+import time
+import datetime
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+
+def get_gold_price():
+    '''
+    返回当前黄金价格
+    '''
+
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--log-level=3')
+    driver = webdriver.Chrome(executable_path=('/usr/bin/chromedriver'), chrome_options=chrome_options)
+
+    for x in range(5):
+        driver.get("http://www.dyhjw.com/hjtd")
+        time.sleep(5)
+        current_html = driver.page_source
+        soup = BeautifulSoup(current_html, 'lxml')
+
+        divs = soup.find(class_='nom last green')
+        if not divs:
+            divs = soup.find(class_='nom last red')
+            if not divs:
+                divs = soup.find(class_='nom last ')
+                if not divs:
+                    print('=' * 20 + str(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))) + '=' * 20)
+                    print(str(soup))
+                    print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())) + '数据获取失败，三分钟后将重试')
+                    time.sleep(180)
+                else:
+                    break
+            else:
+                break
+        else:
+            break
+    driver.quit()
+    if divs:
+        print('price:' + divs.get_text())
+        return float(divs.get_text())
+    else:
+        return None
