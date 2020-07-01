0a752891b0abd1220f572de65711228482e9e6ff
shr1213 <shr1213@live.com>
2020-07-01 18:12:42 +0800

    黄金前端初始化

---
+++ frontend/src/components/Gold.vue
@@ -0,0 +1,44 @@
+<template>
+
+</template>
+<script>
+import axios from "axios";
+import Echarts from 'vue-echarts'
+import 'echarts/lib/chart/line'
+import 'echarts/lib/componet/title'
+import 'echarts/lib/componet/tooltip'
+import 'echarts/lib/componet/legent'
+import WidgetButton from "./common/WidgetButton.vue";
+
+const api = {
+  goldData: "/gold/get",
+};
+
+export default {
+  name: "gold",
+  props: {
+    user_id: Number,
+    widget_id: Number,
+    buttons: Array,
+    flush: Boolean
+  },
+  components: {
+    WidgetButton
+  },
+  watch: {
+    flush(newVal, oldVal) {
+      if (newVal) {
+        this.weatherData();
+      }
+    }
+  },
+  data() {
+  },
+  methods: {
+  },
+  created() {},
+  mounted() {
+  }
+};
+</script>
+</style>
