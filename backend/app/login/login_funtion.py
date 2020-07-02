42b10a9249b8d8cea793c9df015807e3f1e907d3
NolanSun <shr1213@live.com>
2020-07-02 17:54:10 +0800

    20200702

--- backend/app/login/login_funtion.py
+++ backend/app/login/login_funtion.py
@@ -18,6 +18,14 @@ class User(object):
     def __init__(self, user_name='', user_id=0):
         if user_id == 0:
             self.user_name = user_name
+            user_query = user.select().where(user.name == self.user_name).limit(1).dicts()
+            for row in user_query:
+                self.user_id = row['id']
+                self.role_id = row['role_id']
+                self.email = row['email']
+                self.wechat_key = row['wechat_key']
+                self.create_time = row['create_time']
+                self.update_time = row['update_time']
         else:
             self.user_id = user_id
             user_query = user.select().where(user.id == self.user_id).limit(1).dicts()
