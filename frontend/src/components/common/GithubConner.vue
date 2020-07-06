986b2d58ac9ce4bf6d5031147086f3eb9202ad41
NolanSun <shr1213@live.com>
2020-07-06 22:00:47 +0800

    20200706

--- frontend/src/components/common/GithubConner.vue
+++ frontend/src/components/common/GithubConner.vue
@@ -4,7 +4,7 @@
       width="80"
       height="80"
       viewBox="0 0 250 250"
-      style="fill:#70B7FD; color:#fff; position: absolute; top: 0; border: 0; left: 0; transform: scale(-1, 1); z-index: 1;"
+      style="fill:#70B7FD; color:#409EFF; position: absolute; top: 0; border: 0; left: 0; transform: scale(-1, 1); z-index: 1;"
       aria-hidden="true"
     >
       <path d="M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z" />
