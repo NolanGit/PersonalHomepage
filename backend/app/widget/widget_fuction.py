8b0327ba12a46c90d40c740f7a320b875e0320aa
NolanSun <shr1213@live.com>
2020-07-09 00:20:05 +0800

    20200709

--- backend/app/widget/widget_fuction.py
+++ backend/app/widget/widget_fuction.py
@@ -7,13 +7,29 @@ from base_model import Base
 class Widget(Base):
     id = None
     name = None
+    is_valid = None
     span = None
     buttons = None
     auto_update = None
     update_time = None
 
-    def __init__(self, id, name, span, buttons, auto_update, update_time):
-        pass
+    def __init__(self, name=None, is_valid=None, span=None, buttons=None, auto_update=0, update_time=datetime.datetime.now(), id=0):
+        self.name = name
+        self.is_valid = is_valid
+        self.span = span
+        self.buttons = buttons
+        self.auto_update = auto_update
+        self.update_time = update_time
+        self.id = id
+
+    def complete(self):
+        _ = widget_table.get(widget_table.id == id)
+        self.name = _.name
+        self.is_valid = _.is_valid
+        self.span = _.span
+        self.buttons = _.buttons
+        self.auto_update = _.auto_update
+        self.update_time = _.update_time
 
 
 def widget_get(is_valid=1):
--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -12,11 +12,9 @@
       <search :user_id="user_id" />
     </el-row>
     <div class="cardRow">
-      <el-tabs v-model="activeName" @tab-click="handleClick" stretch="true">
+      <el-tabs v-model="activeName" @tab-click="handleClick" stretch="true" style="padding: 0px 20px 30px 20px;">
         <el-tab-pane label="用户管理" name="first"></el-tab-pane>
         <el-tab-pane label="配置管理" name="second"></el-tab-pane>
-        <el-tab-pane label="角色管理" name="third"></el-tab-pane>
-        <el-tab-pane label="定时任务补偿" name="fourth"></el-tab-pane>
       </el-tabs>
       <el-row
         class="margin_bottom-large"
