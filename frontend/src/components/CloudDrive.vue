2cd5b637be2acf8d1dcc8db2ae9b863a336290da
NolanSun <shr1213@live.com>
2020-07-11 15:37:00 +0800

    变为真实数据表格

--- frontend/src/components/CloudDrive.vue
+++ frontend/src/components/CloudDrive.vue
@@ -1,25 +1,37 @@
 <template>
   <el-col>
     <el-row class="margin_bottom-large">
-      <el-upload
-        class="upload-demo"
-        drag
-        action="https://jsonplaceholder.typicode.com/posts/"
-        multiple
-      >
+      <el-upload class="upload" style="text-align: center;" drag action="/upload" multiple>
         <i class="el-icon-upload"></i>
         <div class="el-upload__text">
           将文件拖到此处，或
           <em>点击上传</em>
         </div>
-        <div class="el-upload__tip" slot="tip">只能上传jpg/png文件，且不超过500kb</div>
       </el-upload>
     </el-row>
     <el-row>
-      <el-table :data="tableData" style="width: 100%">
-        <el-table-column prop="date" label="日期" width="180"></el-table-column>
-        <el-table-column prop="name" label="姓名" width="180"></el-table-column>
-        <el-table-column prop="address" label="地址"></el-table-column>
+      <el-table :data="tableData" style="text-align: center;">
+        <el-table-column prop="file_name" label="文件名称" width="180"></el-table-column>
+        <el-table-column prop="update_time" label="上传时间" width="180"></el-table-column>
+        <el-table-column :key="Math.random()" label="操作">
+          <template slot-scope="scope">
+            <el-button
+              v-show="scope.row.is_valid==1"
+              class="noMargin"
+              size="mini"
+              plain
+              @click="roleDisable(scope.row.id)"
+            >下载</el-button>
+            <el-button
+              v-show="scope.row.is_valid==1"
+              class="noMargin"
+              size="mini"
+              plain
+              type="danger"
+              @click="roleDisable(scope.row.id)"
+            >删除</el-button>
+          </template>
+        </el-table-column>
       </el-table>
     </el-row>
   </el-col>
@@ -27,6 +39,7 @@
 <script>
 import axios from "axios";
 
+const FLUSH_INTERVAL = 60000;
 const api = {
   get: "/cloudDrive/get",
   save: "/cloudDrive/save"
@@ -41,22 +54,35 @@ export default {
   watch: {},
   data() {
     return {
-      tableData: [
-        {
-          date: "2016-05-02",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄"
-        }
-      ]
+      tableData: []
     };
   },
-  methods: {},
+  methods: {
+    async get() {
+      try {
+        const { data: res } = await axios.post(api.get, {
+          user_id: this.user_id
+        });
+        this.tableData = res.data;
+        this.$message({
+          message: res["msg"],
+          type: "success"
+        });
+      } catch (e) {
+        console.log(e);
+        this.$message({
+          message: e.response.data.msg,
+          type: "error"
+        });
+      }
+    }
+  },
   mounted() {
-    // this.goldPriceGet();
-    // this.timer = window.setInterval(this.goldPriceGet, this.flush);
+    this.get();
+    this.timer = window.setInterval(this.get, FLUSH_INTERVAL);
   },
   beforeDestroy() {
-    // window.clearInterval(this.timer);
+    window.clearInterval(this.timer);
   }
 };
 </script>
