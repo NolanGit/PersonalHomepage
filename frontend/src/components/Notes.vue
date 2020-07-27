708bca2e9e8ae3491585134ecd7d055a2e5c3c6d
NolanSun <shr1213@live.com>
2020-07-28 00:24:39 +0800

    20200728

--- frontend/src/components/Notes.vue
+++ frontend/src/components/Notes.vue
@@ -6,18 +6,15 @@
       </div>
     </el-row>
     <el-tabs tab-position="left" class="scrollbar-div" style="max-height: 210px;">
-      <el-tab-pane label="用户管理">用户管理</el-tab-pane>
-      <el-tab-pane label="配置管理">配置管理</el-tab-pane>
-      <el-tab-pane label="角色管理">角色管理</el-tab-pane>
-      <el-tab-pane label="定时任务补偿">定时任务补偿</el-tab-pane>
-      <el-tab-pane label="用户管理">用户管理</el-tab-pane>
-      <el-tab-pane label="配置管理">配置管理</el-tab-pane>
-      <el-tab-pane label="角色管理">角色管理</el-tab-pane>
-      <el-tab-pane label="定时任务补偿">定时任务补偿</el-tab-pane>
-      <el-tab-pane label="用户管理">用户管理</el-tab-pane>
-      <el-tab-pane label="配置管理">配置管理</el-tab-pane>
-      <el-tab-pane label="角色管理">角色管理</el-tab-pane>
-      <el-tab-pane label="定时任务补偿">定时任务补偿</el-tab-pane>
+      <el-tab-pane
+        v-for="singleNotesData in notesData"
+        :key="singleNotesData"
+        :label="singleNotesData.name"
+      >
+        <p
+          style="color: #606266; font-size: 12px; font-family: Helvetica Neue,Helvetica,PingFang SC,Hiragino Sans GB,Microsoft YaHei,SimSun,sans-serif;"
+        >{{singleNotesData.content}}</p>
+      </el-tab-pane>
     </el-tabs>
     <el-row type="flex" justify="center" v-show="user_id!=0">
       <WidgetButton
@@ -37,25 +34,43 @@ import axios from "axios";
 import WidgetButton from "./common/WidgetButton.vue";
 
 const api = {
-  get: "/bookmarks/get"
+  get: "/notes/get",
 };
 export default {
   name: "notes",
   props: {
     user_id: Number,
     widget_id: Number,
-    buttons: Array
+    buttons: Array,
   },
   components: {
-    WidgetButton
+    WidgetButton,
   },
   data() {
     return {
-      notestData: [],
+      notesData: [],
     };
   },
-  methods: {},
-  mounted() {}
+  methods: {
+    async notesGet() {
+      try {
+        const { data: res } = await axios.post(api.get, {
+          user_id: this.user_id,
+        });
+        this.notesData = res.data;
+        this.$emit("done");
+      } catch (e) {
+        console.log(e);
+        this.$message({
+          message: e.response.data.msg,
+          type: "error",
+        });
+      }
+    },
+  },
+  mounted() {
+    this.notesGet();
+  },
 };
 </script>
 <style scoped>
