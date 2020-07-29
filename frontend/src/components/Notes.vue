628474b2eaa817e429f18761919c68423a66a375
NolanSun <shr1213@live.com>
2020-07-29 20:17:17 +0800

    20200729

--- frontend/src/components/Notes.vue
+++ frontend/src/components/Notes.vue
@@ -24,7 +24,7 @@
             <div>
               <div>
                 <el-button
-                  @click="edit(singleNotesData.name)"
+                  @click="editClicked(singleNotesData.name)"
                   type="text"
                   style="color:#409EFF"
                   icon="el-icon-edit"
@@ -66,11 +66,11 @@
       <WidgetButton :user_id="user_id" :widget_id="widget_id" :buttons="buttons" @add="add()"></WidgetButton>
     </el-row>
     <el-dialog :title="edit.title" :visible.sync="edit.visible">
-      <el-input v-model="edit.title"></el-input>
-      <el-input v-model="edit.content"></el-input>
+      <el-input size="small" class="margin_bottom-medium" v-model="edit.title"></el-input>
+      <el-input size="small" autosize class="margin_bottom-large" v-model="edit.content"></el-input>
       <div slot="footer" class="dialog-footer">
-        <el-button @click="edit.visible = false">取消</el-button>
-        <el-button type="primary" @click="submit()">确定</el-button>
+        <el-button size="small" @click="edit.visible = false">取消</el-button>
+        <el-button size="small" type="primary" @click="submit()">确定</el-button>
       </div>
     </el-dialog>
   </section>
@@ -138,7 +138,7 @@ export default {
       this.edit.content = "";
       this.edit.visible = true;
     },
-    edit(notesName) {
+    editClicked(notesName) {
       let i = this.notesGetIndex(notesName);
       this.edit.title = this.notesData[i].name;
       this.edit.content = this.notesData[i].content;
