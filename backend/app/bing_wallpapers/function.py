b4f49d5b3dbe1b5c08d96ee5aa8c1bf53acc001f
NolanSun <shr1213@live.com>
2020-09-24 00:41:20 +0800

    完成爬取图片脚本

--- backend/app/bing_wallpapers/function.py
+++ backend/app/bing_wallpapers/function.py
@@ -6,7 +6,7 @@ from werkzeug.utils import secure_filename
 
 cf = configparser.ConfigParser()
 cf.read('app/homepage.config')
-UPLOAD_FILE_PATH = cf.get('config', 'UPLOAD_FILE_PATH')
+BING_WALLPAPERS_PATH = cf.get('config', 'BING_WALLPAPERS_PATH')
 
 result = {}
 base_url = "https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=2"
@@ -19,10 +19,10 @@ result['copyrightlink'] = r.json()['images'][0]['copyrightlink']
 
 img_data = requests.get(url=r['url']).content
 
-folder_path = UPLOAD_FILE_PATH + time.strftime('%Y-%m-%d', time.localtime(time.time()))
+folder_path = BING_WALLPAPERS_PATH
 if not os.path.exists(folder_path):
     os.makedirs(folder_path)
-filename = 'aa.jpg'
+filename = str(result['date']) + '.jpg'
 upload_path = os.path.join(folder_path, secure_filename(filename))  #注意：没有的文件夹一定要先创建，不然会提示没有该路径
 with open(upload_path, 'wb') as fp:
     fp.write(img_data)
