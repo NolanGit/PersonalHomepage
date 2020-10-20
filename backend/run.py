771edd1286a81280a2d82e7a97c2573509ca7435
NolanSun <shr1213@live.com>
2020-10-20 16:40:23 +0800

    Update run.py

--- backend/run.py
+++ backend/run.py
@@ -11,4 +11,4 @@ if __name__ == '__main__':
     except:
         print('使用默认端口50000')
         port = 50000
-    app.run(host='0.0.0.0', port=50000, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=True)
