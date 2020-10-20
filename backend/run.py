7151ec752b886e87425bf3c8402a9886bc5858f5
NolanSun <shr1213@live.com>
2020-10-20 16:37:25 +0800

    Update run.py

--- backend/run.py
+++ backend/run.py
@@ -1,8 +1,14 @@
 #! /usr/bin/env python3
 import os
+import sys
 from app import create_app
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 
 if __name__ == '__main__':
+    try:
+        port = int(sys.argv[1])
+    except:
+        print('使用默认端口50000')
+        port = 50000
     app.run(host='0.0.0.0', port=50000, debug=True)
