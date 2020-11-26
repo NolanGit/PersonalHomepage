20a8cca9d32670f635e96cddfc89b78618baa57e
NolanSun <shr1213@live.com>
2020-11-26 15:03:53 +0800

    bug fix

--- backend/app/news/get_news.py
+++ backend/app/news/get_news.py
@@ -198,7 +198,7 @@ def parse_36kr():
             return (class_text is not None) and ('article-item-title' in class_text)
 
         list = []
-        for soup_a in soup.find_all'li'(class_=article_class_filter):
+        for soup_a in soup.find_all(class_=article_class_filter):
             blist = {}
             hot_name = soup_a.text.replace("\\n", "").replace("\n", "").replace("\\r", "").replace("\r", "").strip()
             hot_url = 'https://36kr.com' + soup_a.get('href')
@@ -231,7 +231,7 @@ def parse_bjnews():
         jsondict["time"] = list_time
 
         list = []
-        for soup_a in soup.find_all'li'('div', 'pin_demo'):
+        for soup_a in soup.find_all('div', 'pin_demo'):
             blist = {}
             hot_name = soup_a.find('a').find('div').text.replace("\\n", "").replace("\n", "").replace("\\r", "").replace("\r", "").strip()
             hot_url = soup_a.find('a').get('href')
@@ -249,7 +249,7 @@ def parse_bjnews():
         list = []
         ranking = soup.find('div', 'ranking')
         ranking['class'] = 'processed'
-        for soup_a in ranking.find_all'li'('a', 'link'):
+        for soup_a in ranking.find_all('a', 'link'):
             blist = {}
             hot_name = soup_a.text.replace("\\n", "").replace("\n", "").replace("\\r", "").replace("\r", "").strip()[2:]
             hot_url = soup_a.get('href')
@@ -266,7 +266,7 @@ def parse_bjnews():
 
         list = []
         ranking = soup.find('div', 'hotComment')
-        for soup_a in ranking.find_all'li'('a', 'link'):
+        for soup_a in ranking.find_all('a', 'link'):
             blist = {}
             hot_name = soup_a.text.replace("\\n", "").replace("\n", "").replace("\\r", "").replace("\r", "").strip()[2:]
             hot_url = soup_a.get('href')
@@ -598,7 +598,7 @@ def parse_douban():
         list_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
         jsondict["time"] = list_time
         jsondict["title"] = "豆瓣热帖"
-        for soup_a in soup.find_all'li'("div", "channel-item"):
+        for soup_a in soup.find_all("div", "channel-item"):
             blist = {}
             hot_name = soup_a.find('h3').find('a').text.replace("\\n", "").replace("\n", "").replace("\\r", "").replace("\r", "").strip()
             hot_url = soup_a.find('h3').find('a').get('href')
@@ -687,7 +687,7 @@ def parse_huxiu():
         def class_filter(class_text):
             return (class_text is not None) and ((class_text == "article-item--large") or ('article-item--normal' in class_text))
 
-        for soup_a in soup.find_all'li'(class_=class_filter):
+        for soup_a in soup.find_all(class_=class_filter):
             blist = {}
             hot_name = soup_a.find('h5').text.replace("\\n", "").replace("\n", "").replace("\\r", "").replace("\r", "").strip()
             hot_url = soup_a.find('a').get('href')
@@ -886,7 +886,7 @@ def parse_bilibili():
         list_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
         jsondict["time"] = list_time
         jsondict["title"] = "Bilibili"
-        for soup_a in soup.find_all'li'("a", "title"):
+        for soup_a in soup.find_all("a", "title"):
             blist = {}
             hot_name = soup_a.text.replace("\\n", "").replace("\n", "").replace("\\r", "").replace("\r", "").strip()
             hot_url = soup_a.get('href')
