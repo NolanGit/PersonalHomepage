c30ab8940f69e5d10bac26e6c2043770d22404a9
NolanSun <shr1213@live.com>
2020-12-07 15:18:33 +0800

    改单例

--- backend/app/model/model_function.py
+++ backend/app/model/model_function.py
@@ -4,18 +4,17 @@ import configparser
 from peewee import Model
 from playhouse.pool import PooledMySQLDatabase
 
-PATH = lambda p: os.path.abspath(os.path.join(os.path.dirname(__file__), p))
-cf = configparser.ConfigParser()
-cf.read(PATH('../homepage.config'))
-DB_PASS = cf.get('config', 'DB_PASS')
-db = PooledMySQLDatabase('PersonalHomepage', user='root', password=DB_PASS, host='localhost', port=3306)
+from ..common_func import singleton
 
 
-class UnknownField(object):
-    def __init__(self, *_, **__):
-        pass
-
-
+@singleton
 class BaseModel(Model):
+
     class Meta:
+        from ..common_func import singleton
+        PATH = lambda p: os.path.abspath(os.path.join(os.path.dirname(__file__), p))
+        cf = configparser.ConfigParser()
+        cf.read(PATH('../homepage.config'))
+        DB_PASS = cf.get('config', 'DB_PASS')
+        db = PooledMySQLDatabase('PersonalHomepage', user='root', password=DB_PASS, host='localhost', port=3306)
         database = db
