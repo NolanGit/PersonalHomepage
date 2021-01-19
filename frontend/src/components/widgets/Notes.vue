ced5eca1fa931e0ccccbc96cf8c3e76b638368fd
NolanSun <shr1213@live.com>
2021-01-19 11:34:53 +0800

    调整样式

--- frontend/src/components/widgets/Notes.vue
+++ frontend/src/components/widgets/Notes.vue
@@ -1,46 +1,65 @@
 <template>
   <section>
-    <el-main class="noPadding" style="height: 300px;">
+    <el-main class="noPadding" style="height: 300px">
       <el-row type="flex" justify="center">
-          <div class="widget-label">便签</div>
+        <div class="widget-label">便签</div>
       </el-row>
-      <el-tabs
-        tab-position="left"
-        class="scrollbar-div"
-        style="max-height: 210px; min-height: 210px;"
-        v-model="activeNote"
-      >
+      <el-tabs tab-position="left" class="scrollbar-div" v-model="activeNote">
         <el-tab-pane
           v-for="singleNotesData in notesData"
           :key="singleNotesData"
           :label="singleNotesData.name"
           :name="singleNotesData.token"
-          style="padding-left:0px;"
+          style="padding-left: 0px"
         >
           <span slot="label">
             <el-dropdown
               @command="actionClicked"
               size="small"
-              v-show="activeNote==singleNotesData.token"
+              v-show="activeNote == singleNotesData.token"
               show-timeout="50"
               placement="bottom"
             >
               <span class="el-dropdown-link">
-                <i class="el-icon-more" style="margin-right: 4px;"></i>
+                <i class="el-icon-more" style="margin-right: 4px"></i>
               </span>
               <el-dropdown-menu slot="dropdown">
-                <el-dropdown-item style="color:#409EFF" command="edit" icon="el-icon-edit">编辑</el-dropdown-item>
-                <el-dropdown-item style="color:#E6A23C" command="bell" icon="el-icon-bell">提醒</el-dropdown-item>
-                <el-dropdown-item style="color:#F56C6C" command="delete" icon="el-icon-delete">删除</el-dropdown-item>
+                <el-dropdown-item
+                  style="color: #409eff"
+                  command="edit"
+                  icon="el-icon-edit"
+                  >编辑</el-dropdown-item
+                >
+                <el-dropdown-item
+                  style="color: #e6a23c"
+                  command="bell"
+                  icon="el-icon-bell"
+                  >提醒</el-dropdown-item
+                >
+                <el-dropdown-item
+                  style="color: #f56c6c"
+                  command="delete"
+                  icon="el-icon-delete"
+                  >删除</el-dropdown-item
+                >
               </el-dropdown-menu>
             </el-dropdown>
-            {{singleNotesData.name}}
+            {{ singleNotesData.name }}
           </span>
-          <p class="notesText">{{singleNotesData.content}}</p>
+          <p
+            class="notesText scrollbar-div"
+            style="max-width: 77%; max-height: 250px; min-height: 250px"
+          >
+            {{ singleNotesData.content }}
+          </p>
         </el-tab-pane>
       </el-tabs>
     </el-main>
-    <el-footer height="31px" style="justify-content: center; display: flex;" v-if="user_id != 0">
+    <el-footer
+      height="31px"
+      style="justify-content: center; display: flex"
+      v-if="user_id != 0"
+    >
       <WidgetButton
         :user_id="user_id"
         :widget_id="widget_id"
@@ -51,12 +70,16 @@
     </el-footer>
 
     <!-- 编辑dialog -->
-    <el-dialog :title="edit.dialogTitle" :visible.sync="edit.visible" :close-on-click-modal="false">
+    <el-dialog
+      :title="edit.dialogTitle"
+      :visible.sync="edit.visible"
+      :close-on-click-modal="false"
+    >
       <div class="div-flex">
         <div class="notesEditFormLabel">标题：</div>
         <el-input
           size="small"
-          style="width: 80%;"
+          style="width: 80%"
           class="margin_bottom-medium"
           v-model="edit.title"
           placeholder="请输入便签标题"
@@ -67,7 +90,7 @@
         <el-input
           type="textarea"
           size="small"
-          style="width: 80%;"
+          style="width: 80%"
           autosize
           class="margin_bottom-large"
           v-model="edit.content"
@@ -76,27 +99,51 @@
       </div>
       <div slot="footer" class="dialog-footer">
         <el-button size="small" @click="edit.visible = false">取消</el-button>
-        <el-button size="small" type="primary" @click="editSubmit()">确定</el-button>
+        <el-button size="small" type="primary" @click="editSubmit()"
+          >确定</el-button
+        >
       </div>
     </el-dialog>
 
     <!-- 推送dialog -->
     <el-dialog title="提醒" :visible.sync="notify.visible">
-      <el-form ref="form" :model="notify.form" size="mini" class="padding_bottom-medium">
+      <el-form
+        ref="form"
+        :model="notify.form"
+        size="mini"
+        class="padding_bottom-medium"
+      >
         <el-form-item label="标题">
           <p
             class="notesText"
-            style="font-size: 14px; padding-top: 0px; margin-top: 0px; line-height: 28px; margin-bottom: 0px"
-          >{{notify.form.title}}</p>
+            style="
+              font-size: 14px;
+              padding-top: 0px;
+              margin-top: 0px;
+              line-height: 28px;
+              margin-bottom: 0px;
+            "
+          >
+            {{ notify.form.title }}
+          </p>
         </el-form-item>
         <el-form-item label="内容">
           <p
             class="notesText"
-            style="font-size: 14px; margin-left: 40px; line-height: 28px; padding-top: 0px; margin-top: 0px; margin-bottom: 0px"
-          >{{notify.form.content}}</p>
+            style="
+              font-size: 14px;
+              margin-left: 40px;
+              line-height: 28px;
+              padding-top: 0px;
+              margin-top: 0px;
+              margin-bottom: 0px;
+            "
+          >
+            {{ notify.form.content }}
+          </p>
         </el-form-item>
         <el-form-item label="提醒方式">
-          <div class="div-flex" style="width:324px">
+          <div class="div-flex" style="width: 324px">
             <el-select
               v-model="notify.form.notifyMethod.select"
               placeholder="请选择"
@@ -125,10 +172,10 @@
             <el-time-select
               v-model="notify.form.triggerTime"
               :picker-options="{
-              start: '00:00',
-              step: '01:00',
-              end: '24:00'
-            }"
+                start: '00:00',
+                step: '01:00',
+                end: '24:00',
+              }"
               placeholder="选择时间"
               size="small"
               class="main_select--medium"
@@ -137,12 +184,24 @@
         </el-form-item>
         <p
           class="notesText"
-          style="font-size: 12px; color: #F56C6C; padding-top: 0px; margin-top: 0px; margin-bottom: 0px"
-        >*提交后不能取消，但可以多次提交。</p>
+          style="
+            font-size: 12px;
+            color: #f56c6c;
+            padding-top: 0px;
+            margin-top: 0px;
+            margin-bottom: 0px;
+          "
+        >
+          *提交后不能取消，但可以多次提交。
+        </p>
       </el-form>
       <span slot="footer" class="dialog-footer">
-        <el-button type="primary" size="small" @click="notify.visible=false">取消</el-button>
-        <el-button type="primary" size="small" @click="notifySubmit()">确定</el-button>
+        <el-button type="primary" size="small" @click="notify.visible = false"
+          >取消</el-button
+        >
+        <el-button type="primary" size="small" @click="notifySubmit()"
+          >确定</el-button
+        >
       </span>
     </el-dialog>
 
@@ -150,19 +209,45 @@
     <el-dialog title="时间机器" :visible.sync="revert.visible">
       <p
         class="notesText"
-        style="font-size: 12px; color: #F56C6C; padding-top: 0px; margin-top: 0px; margin-bottom: 0px"
-      >*可以恢复到最近的五个版本中的任意一个，此操作不会对过去的版本产生影响，而是会使用以前版本的内容生成一个新版本。</p>
-      <el-table :data="revert.data" style="text-align: center; margin-bottom: 20px;" size="small">
+        style="
+          font-size: 12px;
+          color: #f56c6c;
+          padding-top: 0px;
+          margin-top: 0px;
+          margin-bottom: 0px;
+        "
+      >
+        *可以恢复到最近的五个版本中的任意一个，此操作不会对过去的版本产生影响，而是会使用以前版本的内容生成一个新版本。
+      </p>
+      <el-table
+        :data="revert.data"
+        style="text-align: center; margin-bottom: 20px"
+        size="small"
+      >
         <el-table-column prop="update_time" label="版本"></el-table-column>
-        <el-table-column prop="user" label="创建人" width="100"></el-table-column>
+        <el-table-column
+          prop="user"
+          label="创建人"
+          width="100"
+        ></el-table-column>
         <el-table-column :key="Math.random()" label="操作" width="150">
           <template slot-scope="scope">
             <el-popover placement="right" width="350" trigger="hover">
-              <el-table :data="scope.row.detail" style="text-align: center;" size="small">
-                <el-table-column prop="name" width="100" label="标题"></el-table-column>
+              <el-table
+                :data="scope.row.detail"
+                style="text-align: center"
+                size="small"
+              >
+                <el-table-column
+                  prop="name"
+                  width="100"
+                  label="标题"
+                ></el-table-column>
                 <el-table-column label="内容">
                   <template slot-scope="innerScope">
-                    <span v-if="innerScope.row.content.length<25">{{ innerScope.row.content }}</span>
+                    <span v-if="innerScope.row.content.length < 25">{{
+                      innerScope.row.content
+                    }}</span>
                     <el-popover
                       placement="top"
                       width="200"
@@ -171,8 +256,11 @@
                     >
                       <span
                         slot="reference"
-                        v-if="innerScope.row.content.length>=25"
-                      >{{ innerScope.row.content.substring(0,25)+'...' }}</span>
+                        v-if="innerScope.row.content.length >= 25"
+                        >{{
+                          innerScope.row.content.substring(0, 25) + "..."
+                        }}</span
+                      >
                     </el-popover>
                   </template>
                 </el-table-column>
@@ -184,7 +272,8 @@
                 type="primary"
                 slot="reference"
                 @click="revertConfirm(scope.row.update_time, scope.row.detail)"
-              >恢复至此版本</el-button>
+                >恢复至此版本</el-button
+              >
             </el-popover>
           </template>
         </el-table-column>
