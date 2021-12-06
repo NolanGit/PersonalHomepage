e462d429158be208c9507e7d6efc8122b5baebf5
Nolan Sun <shr1213@live.com>
2021-12-06 20:03:48 +0800

    bug fix

--- backend/app/model/model_function.py
+++ backend/app/model/model_function.py
@@ -13,6 +13,7 @@ from config_helper import ConfigHelper
 class BaseDb():
 
     def __init__(self):
+        config_helper = ConfigHelper()
         PATH = lambda p: os.path.abspath(os.path.join(os.path.dirname(__file__), p))
         DB_PASS = config_helper.get('DB_PASS')
         DB_HOST = config_helper.get('DB_HOST')
