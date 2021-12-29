fd6c06459c2aa4d2a5a4a5dc498c8210b8e40b6d
shr1213 <shr1213@live.com>
2021-12-29 19:33:47 +0800

    bug fix

--- backend/app/model/model_function.py
+++ backend/app/model/model_function.py
@@ -12,7 +12,6 @@ class BaseDb():
 
     def __init__(self):
         config_helper = ConfigHelper()
-        PATH = lambda p: os.path.abspath(os.path.join(os.path.dirname(__file__), p))
         DB_PASS = config_helper.get('DB_PASS')
         DB_HOST = config_helper.get('DB_HOST')
         DB_USER = config_helper.get('DB_USER')
