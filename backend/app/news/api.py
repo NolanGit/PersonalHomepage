e4fe12fa1fd0a034740a7aad6f7571c1dc4936f0
Nolan Sun <shr1213@live.com>
2022-01-28 00:25:39 +0800

    性能

--- backend/app/news/api.py
+++ backend/app/news/api.py
@@ -155,7 +155,7 @@ def search():
         start_time = data['start_date'] + ' 00:00:00'
         end_time = data['end_date'] + '59:59:59'
 
-        _ = news_table.select().where((news_table.content.contains(keyword)) & (news_table.create_time >= start_time) & (news_table.create_time <= end_time)).dicts()
+        _ = news_table.select().where((news_table.create_time >= start_time) & (news_table.create_time <= end_time)).where(news_table.content.contains(keyword)).dicts()
         r = [{'website': __['website'], 'category': __['category'], 'content': __['content'], 'create_time': __['create_time'], } for __ in _]
         return rsp.success(r)
     except Exception as e:
